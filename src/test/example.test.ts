69b01285eb9fcd6a792fb65f0472843bffba915f
Tarun <tarun.sharma@tarun.sharma.macbook>
2021-10-12 16:08:46 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,8 +1,7 @@
 import {Example} from "../main/example";
 
-describe('example test test', () => {
-    it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
+describe('FizzBuzzKata test', () => {
+    it("should give output number as string", () => {
+        expect("4").toBe("4");
     })
 })
