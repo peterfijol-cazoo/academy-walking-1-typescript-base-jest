d8b4e7315d2d7f70a724c04257828d2ed78c3b13
Peter Fijol <peter.fijol@cazoo.co.uk>
2021-10-12 16:09:59 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,7 +1,14 @@
 import {Example} from "../main/example";
 
+<<<<<<< Updated upstream
 describe('FizzBuzzKata test', () => {
     it("should give output number as string", () => {
         expect("4").toBe("4");
+=======
+describe('test', () => {
+    it("should give sum of a + b when adding a and b", () => {
+        let example: Example = new Example();
+        expect(example.add(1, 2)).toBe(3);
+>>>>>>> Stashed changes
     })
 })
