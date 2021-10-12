75e4a30104603592b62c3432e3d98a2fbb15fba5
Peter Fijol <peter.fijol@cazoo.co.uk>
2021-10-12 16:23:02 +0100

    mob next [ci-skip] [ci skip] [skip ci]

--- src/test/example.test.ts
+++ src/test/example.test.ts
@@ -1,14 +1,19 @@
-import {Example} from "../main/example";
+export class Fizz {
+    do(digit: number) {
+        return digit % 3 == 0 ? "Fizz" : digit.toString();
+    }
+}
 
-<<<<<<< Updated upstream
 describe('FizzBuzzKata test', () => {
     it("should give output number as string", () => {
         expect("4").toBe("4");
-=======
-describe('test', () => {
-    it("should give sum of a + b when adding a and b", () => {
-        let example: Example = new Example();
-        expect(example.add(1, 2)).toBe(3);
->>>>>>> Stashed changes
-    })
+    });
+
+    it("should give Fizz if multiples of three", () => {
+        expect(new Fizz().do(3)).toBe("Fizz");
+    });
+
+    it("should give Buzz if multiples of five", () => {
+        expect(new Fizz().do(3)).toBe("Buzz");
+    });
 })
